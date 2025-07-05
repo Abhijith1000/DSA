2861e4a1b775a10a77db49971f4ebc1ddfe596b7
Abhijith <abhijithgangadharan.mec@gmail.com>
2025-07-06 03:03:59 +0530

    added two sum

--- twosum.py
+++ twosum.py
@@ -1,15 +1,9 @@
 class Solution:
-	def twoSum(self, arr, target):
-	    f=0
-        for i in range(len(arr)):
-		    for j in range(1,len(arr)):
-		        if arr[i]+arr[j]==target and i!=j:
-		            f=1
-		            break
-	        if f==1:
-		           break
-		        
-        if f==1:
-		    return True
-	    else:
-	        return False
+    def twoSum(self, arr, target):
+        seen=set()
+        for i in arr:
+            if target - i in seen:
+                return True
+            else:
+                seen.add(i)
+        return False
